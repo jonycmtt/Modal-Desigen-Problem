1a483cbdcabc90ed1cb864c0c5c5fc3079848f8a
Eftikharuzzaman <ekjraka621729@gmail.com>
2023-03-16 16:12:22 +0600

    Modal Desigen Problem

---
+++ main.js
@@ -0,0 +1,5 @@
+// let modal = document.querySelector('.showTotalmodal')
+// document.getElementById('.showModal')
+// addEventListener('click',function(){
+//     modal.classList.add('showTotalmodal')
+// })
